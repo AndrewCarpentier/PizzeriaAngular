45daed3a2bb03e795b6406c0878419db31a9acc8
selim <selimsahraoui@gmail.com>
2019-04-18 11:37:08 +0200

    variables Service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,5 +3,10 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class DataService {
 
+  pizzasCommand = [];
+  commands = [];
+  
   constructor() { }
+
+
 }
--- src/app/list-pizza/list-pizza.component.ts
+++ src/app/list-pizza/list-pizza.component.ts
@@ -10,7 +10,7 @@ export class ListPizzaComponent implements OnInit {
 
 
   pizzas = [{id: 1, name :"4 fromages" , description:" description de la pizza fromages ", price:"5€", img:"https://www.dominos.fr/ManagedAssets/FR/product/P4FR/FR_P4FR_fr_hero_2142.png?v2008054620"},
-  {id: 1, name :"4 fromages" , description:" description de la pizza fromages ", price:"5€", img:"https://www.dominos.fr/ManagedAssets/FR/product/P4FR/FR_P4FR_fr_hero_2142.png?v2008054620"}]
+  {id: 2, name :"4 saisons" , description:" description de la pizza fromages ", price:"5€", img:"https://www.dominos.fr/ManagedAssets/FR/product/P4FR/FR_P4FR_fr_hero_2142.png?v2008054620"}]
   
   constructor() { }
 
